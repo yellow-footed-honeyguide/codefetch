9440a7704324519e7cb2e9105e1f9cf2b92e2e93
Sergey Veneckiy <s.venetsky@gmail.com>
2024-09-23 19:35:01 +0300

    Nicer

---
+++ build/CMakeFiles/codefetch.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/codefetch.dir/codefetch.cpp.o"
+  "CMakeFiles/codefetch.dir/codefetch.cpp.o.d"
+  "codefetch"
+  "codefetch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/codefetch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
