4952433db244aa3f6052410c0dd45b46e6620109
Sergey Veneckiy <s.venetsky@gmail.com>
2024-09-24 16:24:49 +0300

    fix: Add special files set to is_source_file function in order to fix bug in Metabuild System module

--- src/file_utils.cpp
+++ src/file_utils.cpp
@@ -1,28 +1,40 @@
 #include "file_utils.hpp"
 #include <functional>
 #include <algorithm>
+#include <unordered_set>
 
 namespace FileUtils {
-
-bool is_source_file(const fs::path& path) {
-    static const std::vector<std::string> extensions = {
-    ".c", ".cpp", ".cxx", ".cc", ".h", ".hpp", ".hxx", ".py", ".java",
-    ".js", ".ts", ".rb", ".php", ".go", ".rs", ".swift", ".kt",
-    ".vim", ".txt", ".pl", ".mk", ".sh", ".m4", ".yaml", ".yml", ".md",
-    ".license", ".def", ".awk", ".html", ".rc", ".lua", ".csh", ".cs",
-    ".el", ".forth", ".idl", ".json", ".m", ".svg", ".sv", ".wl", ".xml",
-    ".dts", ".dtsi", ".s", ".S", ".asm", ".bash", ".pl", ".dot", ".csv",
-    ".xsl", ".y", ".l", ".ld", ".cmake", ".ac", ".uc", ".smk", ".css",
-    ".service", ".bzl", ".hex", ".ini", ".sed", ".feature", ".mat", ".rb",
-    ".toml", ".tex", ".xsd", ".proto", ".cf", ".dockerfile", ".ps1",
-    ".gotmpl", ".jsonl", ".zsh", ".hh", ".hpp", ".hxx",
-    ".mm", ".scala", ".groovy", ".f", ".f90", ".f95", ".hs",
-    ".jl", ".lisp", ".lsp", ".pas", ".r", ".sql", ".tcl", ".vb", ".vhdl",
+  bool is_source_file(const fs::path& path) {
+    static const std::vector<std::string> extensions = { 
+        ".c", ".cpp", ".cxx", ".cc", ".h", ".hpp", ".hxx", ".py", ".java",
+        ".js", ".ts", ".rb", ".php", ".go", ".rs", ".swift", ".kt",
+        ".vim", ".txt", ".pl", ".mk", ".sh", ".m4", ".yaml", ".yml", ".md",
+        ".license", ".def", ".awk", ".html", ".rc", ".lua", ".csh", ".cs",
+        ".el", ".forth", ".idl", ".json", ".m", ".svg", ".sv", ".wl", ".xml",
+        ".dts", ".dtsi", ".s", ".S", ".asm", ".bash", ".pl", ".dot", ".csv",
+        ".xsl", ".y", ".l", ".ld", ".cmake", ".ac", ".uc", ".smk", ".css",
+        ".service", ".bzl", ".hex", ".ini", ".sed", ".feature", ".mat", ".rb",
+        ".toml", ".tex", ".xsd", ".proto", ".cf", ".dockerfile", ".ps1",
+        ".gotmpl", ".jsonl", ".zsh", ".hh", ".hpp", ".hxx",
+        ".mm", ".scala", ".groovy", ".f", ".f90", ".f95", ".hs",
+        ".jl", ".lisp", ".lsp", ".pas", ".r", ".sql", ".tcl", ".vb", ".vhdl",
     };
+
+    static const std::unordered_set<std::string> special_files = {
+        "WORKSPACE", "BUILD.bazel", "meson.build", "CMakeLists.txt", "configure.ac", "Makefile"
+    };
+
+    std::string filename = path.filename().string();
+    if (special_files.find(filename) != special_files.end()) {
+        return true;
+    }
+
     std::string ext = path.extension().string();
     std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
     return std::find(extensions.begin(), extensions.end(), ext) != extensions.end();
 }
+
+
 
 void traverse_directory(const fs::path& dir_path, ThreadSafeQueue& file_queue, std::atomic<size_t>& total_files, int max_depth) {
     std::function<void(const fs::path&, int)> traverse = [&](const fs::path& path, int depth) {
