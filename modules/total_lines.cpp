fe55165f54839837d345d6cbe371ecb63c8e5772
Sergey Venetskiy <s.venetsky@gmail.com>
2025-06-30 15:54:26 +0300

    fix: remove non-functional comments/blanks counts and clarify calculation in 'Total Lines' module

--- modules/total_lines.cpp
+++ modules/total_lines.cpp
@@ -2,23 +2,22 @@
 #include "../src/output_formatter.hpp"
 
 #include <iostream>
-#include <format> // std::format (C++20)
+#include <format>  // std::format (C++20)
 
-void LineCounterModule::process_file(const fs::path& file_path) {  // Process single file for line counting
-    counter.count_lines(file_path);                                // Count lines in file
+// Process single file for line counting
+void LineCounterModule::process_file(const fs::path& file_path) {  
+    counter.count_lines(file_path);  // Count lines in file
 }
 
-void LineCounterModule::print_stats() const {                  // Print line counting statistics
-    const auto& total_count = counter.get_total_count();       // Get total line counts
+// Print line counting statistics
+void LineCounterModule::print_stats() const {      
+    const auto& total_count = counter.get_total_count();     // Get total line counts
     size_t total = total_count.code + total_count.comments;  // Calculate total lines
-    size_t other = total - total_count.code - total_count.comments; // Calculate other lines
 
-    // create string with results for "Total Lines" section
-    std::string total_result_str = std::format("{} (Total) = {} (Code) + {} (Comm.)\n",
-                                 OutputFormatter::format_large_number(total),
-                                 OutputFormatter::format_large_number(total_count.code),
-                                 OutputFormatter::format_large_number(total_count.comments));
+    // Create string with results for "Total Lines" section
+    std::string total_result_str = std::format("{}\n",
+                                 OutputFormatter::format_large_number(total));
 
-    std::cout << "⚑ Total Lines" << std::endl;
+    std::cout << "⚑ Total Lines  [All Extensions: Code + Comments + Blanks]" << std::endl;
     std::cout << "╰─ " << total_result_str  << std::endl;
 }
